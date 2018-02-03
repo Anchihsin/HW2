03856c80abea3ea7f9b0c056adfac41d8cafe8a3
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-02-02 20:37:50 -0800

    Update assignment-3.js

--- assignment-3.js
+++ assignment-3.js
@@ -4,6 +4,6 @@ colorMode(HSB, 100);
 
 while(true) {
   background(0, 0, 255, 0.1);
-  fill(50, random(10,300), 200);
+  fill(100, random(5,500), 300);
   ellipse(random(width), random(height), random(20, 40));
 }
