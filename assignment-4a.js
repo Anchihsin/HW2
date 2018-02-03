1fc05f1aa50bb435e018e079af2f8896df8c0288
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-02-02 21:10:06 -0800

    Update assignment-4a.js

--- assignment-4a.js
+++ assignment-4a.js
@@ -1,1 +1,39 @@
+background(340);
+
+stroke(100);
+strokeWeight(5);
+rect(0,0, 320, 300);
+
+//left white
+noFill();
+stroke(0);
+rect(0, 0, 30, 50);
+
+//left red
+fill(255,0,0);
+rect(0, 50, 30, 100);
+
+//left blue
+fill(0,100,255);
+rect(0, 150, 60, 150);
+
+//mid yellow
+fill(255, 204, 0);
+rect(30, 0, 150, 150);
+
+//mid black
+fill(0,0,0);
+rect(60, 150, 130, 110);
+
+
+//right black
+rect(180, 0, 60, 60);
+
+//right white
+noFill();
+rect(180, 60, 140, 90);
+
+//right red
+fill(255,0,0)
+rect(190,150,100,150);
 
